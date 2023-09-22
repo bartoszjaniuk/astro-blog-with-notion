fcaeecc982669f4f1914570fab4c4b15dbac484c
Bartosz <bartosz.janiuk@tsh.io>
2023-09-22 13:05:38 +0200

    add postcss, postcss-import to adjust usage of tailwind, add base seo component and layout for page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00BFA6",
+        secondary: "#00ab95",
+        softBg: "#fafafa",
+      },
+    },
+  },
+  plugins: [],
+};
