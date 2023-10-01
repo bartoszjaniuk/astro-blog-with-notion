3fbe42a237a6bfd4f6a6a682f1750fc1fa1f48ee
Bartosz <bartosz.janiuk@tsh.io>
2023-10-01 10:35:13 +0200

    fix navbar

--- src/layouts/components/navbar/Navbar.tsx
+++ src/layouts/components/navbar/Navbar.tsx
@@ -3,8 +3,9 @@ import ResponsiveLinksWithHamburger from "../responsiveLinksWithHamburger/Respon
 
 const Navbar = () => {
 	return (
-		<nav className="container responsive-padding mx-auto  h-24  flex items-center justify-between">
-			{/* <div className="social">
+		<div className="bg-[RGBA(255,255,255,0.95)] w-full fixed z-50">
+			<nav className="container responsive-padding mx-auto  h-24  flex items-center justify-between ">
+				{/* <div className="social">
 				<button>
 					<Icon
 						width={ICON_SIZE}
@@ -20,36 +21,37 @@ const Navbar = () => {
 					/>
 				</button>
 			</div> */}
-			<div className="logo text-primary font-headings font-bold">
-				Czasopismo Stomatologiczne
-			</div>
-			<ResponsiveLinksWithHamburger />
-			<div className="links">
-				{MENU_LINKS.map((link, index) => {
-					if (link.title === "Newsletter") {
+				<div className="logo text-primary font-headings font-bold w-full">
+					Czasopismo Stomatologiczne
+				</div>
+				<ResponsiveLinksWithHamburger />
+				<div className="links">
+					{MENU_LINKS.map((link, index) => {
+						if (link.title === "Newsletter") {
+							return (
+								<a
+									className="text-primary font-semibold md:text-base"
+									key={index}
+									href={link.path}
+									onClick={(e) => e.preventDefault()}
+								>
+									{link.title}
+								</a>
+							);
+						}
 						return (
 							<a
-								className="text-primary font-semibold md:text-base"
+								className="relative text-xl md:text-base w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-primary after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-500 after:origin-center whitespace-nowrap	"
 								key={index}
 								href={link.path}
-								onClick={(e) => e.preventDefault()}
 							>
 								{link.title}
 							</a>
 						);
-					}
-					return (
-						<a
-							className="relative text-xl md:text-base w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-primary after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-500 after:origin-center whitespace-nowrap	"
-							key={index}
-							href={link.path}
-						>
-							{link.title}
-						</a>
-					);
-				})}
-			</div>
-		</nav>
+					})}
+				</div>
+			</nav>
+		</div>
 	);
 };
 
--- src/views/homepage/components/sections/Sections.tsx
+++ src/views/homepage/components/sections/Sections.tsx
@@ -3,7 +3,7 @@ export const Sections = () => {
 		<section className="bg-softBg border h-full">
 			<div
 				id="cards-section"
-				className="container mx-auto h-auto py-8 lg:h-[500px] items-center grid grid-cols-2 lg:grid-cols-4 gap-8 responsive-padding "
+				className="container mx-auto h-auto py-8 lg:h-[500px] items-center grid grid-cols-1 lg:grid-cols-4 gap-8 responsive-padding "
 			>
 				<div className="h-[350px] shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] p-4 relative">
 					<span className="card-background" />
