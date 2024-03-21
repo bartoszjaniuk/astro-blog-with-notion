6d0e4dc24c7194e4a25f1111be0af433e5861abd
Bartosz <bartosz.janiuk@tsh.io>
2024-03-21 17:02:50 +0100

    add banner into recommendations

--- src/views/polecamy/Polecamy.tsx
+++ src/views/polecamy/Polecamy.tsx
@@ -3,12 +3,20 @@ import { PageLayout } from "@shared/pageLayout/PageLayout";
 export const PolecamyPage = () => {
 	return (
 		<PageLayout>
-			<main className="container p-4">
+			<main className="container p-4 flex flex-col gap-4">
 				<a href="https://www.dentalmedicashow.pl">
 					<img
 						className="transition duration-300 ease-in-out hover:scale-105"
 						src="/assets/polecamy/dentalMedicaShow.jpg"
 						alt="Dental Medica Show"
+					/>
+				</a>
+
+				<a href="acteongroup.com">
+					<img
+						className="transition duration-300 ease-in-out hover:scale-105"
+						src="/assets/polecamy/ACTEON_BANER_PRIME_3D_MARZEC_2024.png"
+						alt="Acteon Group"
 					/>
 				</a>
 			</main>
