6125db7af8130b618944522a61405bd500172934
Bartosz <bartosz.janiuk@tsh.io>
2024-06-25 21:36:31 +0200

    add next release

--- src/views/aktualny-numer/AktualnyNumerPage.tsx
+++ src/views/aktualny-numer/AktualnyNumerPage.tsx
@@ -14,14 +14,14 @@ export const AktualnyNumerPage = () => {
 						<Tilt transitionSpeed={2000} gyroscope={true}>
 							<img
 								className="w-full h-auto"
-								src="/images/currentNumber/czasopismo-pierwsza-strona.png"
+								src="/images/currentNumber/wydanie2.png"
 								alt="czasopismo-pierwsza-strona"
 							/>
 						</Tilt>
 					</a>
 					<a
 						className="text-primary text-2xl font-bold lg:text-5xl xl:text-6xl underline"
-						href="/wydania/wydanie1.pdf"
+						href="/wydania/wydanie2.pdf"
 						target="_blank"
 					>
 						Kliknij tutaj, aby wyświetlić czasopismo
